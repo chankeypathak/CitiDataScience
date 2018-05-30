49970fac2edc4a85e29087344803cf781e599da1
bbrelin <bbrelin@gmail.com>
2018-05-30 09:52:43 +0100

    First draft of Module 3 slides

--- test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import matplotlib.pyplot as plt
-
-datalist = [1,2,3,4,5]
-plt.plot(datalist)
-plt.ylabel('datalist elements')
-plt.show()
--- test2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import matplotlib.pyplot as plt
-
-y_datalist = [1,2,3,4,5]
-x_datalist = [0.5,0.6,0.7,0.8,0.9]
-plt.plot(x_datalist,y_datalist,'r')
-plt.ylabel('datalist elements')
-plt.show()
